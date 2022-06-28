2fea9650bf6a831efaac6e3a5a6d243ea04b886f
schm00g <hk8igureu@relay.firefox.com>
2022-06-29 00:23:23 +0100

    endpoint for brewed before date

--- src/services/http/index.js
+++ src/services/http/index.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 const API_URL = "https://api.punkapi.com/v2/beers";
 const headers = { "Content-Type": "application/json" };
+const MAX_ABV_FOR_NON_ALCOHOLIC_BEER = 2;
 
 const getAllBeers = () => {
   return axios.get(`${API_URL}`, { headers });
 };
 
 const getAllNonAlcoholicBeers = () => {
-  return axios.get(`${API_URL}?abv_lt=2`, { headers });
+  return axios.get(`${API_URL}?abv_lt=${MAX_ABV_FOR_NON_ALCOHOLIC_BEER}`, {
+    headers,
+  });
 };
 
-export { getAllBeers, getAllNonAlcoholicBeers };
+const getAllBeersBrewedBeforeDate = (date) => {
+  return axios.get(`${API_URL}?brewed_before=${date}`, { headers });
+};
+
+export { getAllBeers, getAllNonAlcoholicBeers, getAllBeersBrewedBeforeDate };
