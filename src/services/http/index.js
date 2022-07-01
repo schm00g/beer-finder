6641f6b6e02e58a8f2c7fb2980e1660446535d0b
schm00g <hk8igureu@relay.firefox.com>
2022-07-01 18:11:47 +0100

    80 per page

--- src/services/http/index.js
+++ src/services/http/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 const API_URL = "https://api.punkapi.com/v2/beers";
 const headers = { "Content-Type": "application/json" };
-// TODO: what is definition of non alcoholic beer
 const MAX_ABV_FOR_NON_ALCOHOLIC_BEER = 1;
 
 const getAllBeers = () => {
-  return axios.get(`${API_URL}`, { headers });
+  // TODO: pagination
+  return axios.get(`${API_URL}?per_page=80`, { headers });
 };
 
 const getAllNonAlcoholicBeers = () => {
